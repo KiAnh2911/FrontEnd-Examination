7f34bf514a0d99e8a9766a249d80101274d8c42e
unknown <matic2kaa@gmail.com>
2022-11-26 18:55:50 +0700

    project

--- js/index.js
+++ js/index.js
@@ -7,3 +7,9 @@ $(".lightbox").on("click", function () {
   $("header .wrp nav").hide();
   $(".lightbox").hide();
 });
+
+$(".all .simplify .text .bottom .btn").on("click", function () {
+  if ($(this).hasClass(".active")) return;
+  $(".all .simplify .text .bottom .btn").removeClass("active");
+  $(this).toggleClass("active");
+});
